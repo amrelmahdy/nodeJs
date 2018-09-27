f6491dee588c453fb12a62516cf0f12e3f549caa
Amr El Mahdy <amrelmhdy@hotmail.com>
2018-09-27 19:48:56 +0200

    protypes

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var handler = function(callback){
-  console.log("here is handling function")
-};
+function Player() {
+    this.name = "";
+    this.life = 100;
+    this.giveLife = function (targetPlayer) {
+        targetPlayer.life += 1;
+        console.log(this.name + " gave you a life to " + targetPlayer.name);
+    }
+}
 
-var callback = setTimeout(function () {
-    console.log("here is call back")
-}, 500);
+var amr = new Player()
+var mohsen = new Player()
 
-handler(callback);
+amr.name = "Amr"
+mohsen.name = "Mohsen"
+
+amr.giveLife(mohsen)
+
+console.log(amr.life)
+console.log(mohsen.life)
